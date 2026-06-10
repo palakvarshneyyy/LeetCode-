da21263b1857f39eaf04bd566e37c8784c33f267
Palak Varshney <varshneypalak11@gmail.com>
2026-06-10 22:27:05 +0530

    Week_2 Day3 Solutions

---
+++ Week_2/Day_3/RepeatedSubstringPattern.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for(int l = 1; l<= n/2; l++){
+            if(n % l == 0){
+                String pattern = s.substring(0, l);
+                StringBuilder sb = new StringBuilder();
+                int t = n/l;
+                for(int i=0; i<t; i++){
+                    sb.append(pattern);
+                }
+                if(sb.toString().equals(s)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
